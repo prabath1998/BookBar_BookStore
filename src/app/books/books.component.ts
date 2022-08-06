dd43fec75ebf00a1b59ecfab7fd6460f5438a245
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-06 23:37:02 +0530

    some improvements

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Book {
+  name:string;
+  author:string;
+  image:string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  books:Book[] = [
+    {
+      name: 'Clean Code',
+      author: 'Robert C Martin',
+      image: 'https://cdn.lifehack.org/wp-content/uploads/2015/03/h2g2-01-copy.png',
+    },
+    {
+      name: 'Testing Computer Software',
+      author: 'Robert C Martin',
+      image: 'https://simpleprogrammer.com/wp-content/uploads/2015/03/testing-computer-software.jpg',
+    },
+  ];
 
-  name = 'Clean Code';
-  author = 'Robert C Martin'
+  isShowing: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleBooks() {
+    this.isShowing = !this.isShowing;
   }
-
 }
